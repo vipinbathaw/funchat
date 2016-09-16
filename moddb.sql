b2037e753ff1c030a5b45a1e47f463ece149141b
Vipin Bathaw <gamesforvipin@gmail.com>
2016-09-16 17:29:41 +0530

    Remove unwanted temporary files

--- moddb.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS notifications (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  message varchar(255) NOT NULL,
-  checked int(11) NOT NULL DEFAULT '0',
-  nfor int(11) NOT NULL,
-  PRIMARY KEY (id)
-);
